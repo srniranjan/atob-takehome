27360c4cd1964144a50711f85d013eac1bcda27f
Niranjan Salimath <niranjan.salimath@gmail.com>
2021-06-20 12:09:36 +0530

    Added google maps react component

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
 
 function App() {
+  const props = {
+      center: {lat: 40.73, lng: -73.93}, 
+      zoom: 12
+  }
+
+  const AnyReactComponent = ({ text }) => <div>{text}</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDYSiy0JBx9SqSsahYAe3wDecvX2JYaOyo' }}
+        defaultCenter={props.center}
+        defaultZoom={props.zoom}
+        onGoogleApiLoaded={({map, maps}) => {let marker = new maps.Marker({
+          position: { lat: 40.73, lng: -73.93 },
+          map,
+          title: 'Hello World!'
+          });
+          return marker;
+         }}
+        yesIWantToUseGoogleMapApiInternals
+      />
     </div>
   );
 }

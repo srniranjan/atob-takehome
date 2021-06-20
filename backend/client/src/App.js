585b8813b6ac16b3dad3f033cccc0c28c20c6c95
Niranjan Salimath <niranjan.salimath@gmail.com>
2021-06-20 12:30:17 +0530

    Added firebase to the project and implemented a Google login

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,5 +1,34 @@
 import './App.css';
 import GoogleMapReact from 'google-map-react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD4F5OZb1NTtkY422pe58CpQFd5wlgIFZU",
+  authDomain: "atob-takehome.firebaseapp.com",
+  projectId: "atob-takehome",
+  storageBucket: "atob-takehome.appspot.com",
+  messagingSenderId: "117907608442",
+  appId: "1:117907608442:web:06630c7df7f1af5725bcc3"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
 
 function App() {
   const props = {
@@ -7,11 +36,11 @@ function App() {
       zoom: 12
   }
 
-  const AnyReactComponent = ({ text }) => <div>{text}</div>
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App" style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
+      {user ? <GoogleMapReact
         bootstrapURLKeys={{ key: 'AIzaSyDYSiy0JBx9SqSsahYAe3wDecvX2JYaOyo' }}
         defaultCenter={props.center}
         defaultZoom={props.zoom}
@@ -23,7 +52,7 @@ function App() {
           return marker;
          }}
         yesIWantToUseGoogleMapApiInternals
-      />
+      /> : <SignIn />}
     </div>
   );
 }
